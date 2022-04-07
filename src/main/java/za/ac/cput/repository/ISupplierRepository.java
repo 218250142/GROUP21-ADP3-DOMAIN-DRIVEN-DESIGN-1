eb7e3fb16b2842e5c379616575a1b45af2731a68
218290276 <80412082+218290276@users.noreply.github.com>
2022-04-07 22:08:23 +0200

    Repository Repository Test

---
+++ src/main/java/za/ac/cput/repository/ISupplierRepository.java
@@ -0,0 +1,20 @@
+
+/* Supplier java
+        IRepository for Supplier
+        Author: Aristoteles Pascoal (218290276)
+        Date: 29 March 2022
+        */
+
+package za.ac.cput.repository;
+import za.ac.cput.entity.Supplier;
+import java.util.Set;
+
+public interface ISupplierRepository {
+    Supplier create(Supplier supplier);
+
+    Supplier read(String id);
+
+    Supplier update(Supplier supplier);
+
+    public Set<Supplier> getAll();
+}
