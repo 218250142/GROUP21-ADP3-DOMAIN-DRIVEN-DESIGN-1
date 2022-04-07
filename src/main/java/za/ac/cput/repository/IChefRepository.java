b9926c44dd878024400d6b639ad8bedf0f4ed0b7
218290276 <80412082+218290276@users.noreply.github.com>
2022-04-07 22:29:34 +0200

    2182902796-Repository Fix

--- src/main/java/za/ac/cput/repository/IChefRepository.java
+++ src/main/java/za/ac/cput/repository/IChefRepository.java
@@ -6,11 +6,7 @@ import za.ac.cput.repository.IRepository;
 import java.util.Set;
 
 public interface IChefRepository extends IRepository <Chef,String > {
-    Chef create(Chef chef);
 
-    Chef read(String id);
-
-    Chef update(Chef chef);
 
     public Set<Chef> getAll();
 }
